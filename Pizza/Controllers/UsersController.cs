48fbf4c1511df2509dfd124640271c691ac515a1
MosCs <mosfox.fep@gmail.com>
2023-01-20 17:36:40 +0700

    first commit

---
+++ Pizza/Controllers/UsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pizza.Models;
+
+namespace Pizza.Controllers
+{
+    public class UsersController : Controller
+    {
+        private PizzaNewEntities db = new PizzaNewEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var user = db.User.Include(u => u.Title);
+            return View(user.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.User_TitleID = new SelectList(db.Title, "Title_ID", "Title_Name");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "User_ID,User_Name,User_Password,User_TitleID,User_Address,User_Phone")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.User_TitleID = new SelectList(db.Title, "Title_ID", "Title_Name", user.User_TitleID);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_TitleID = new SelectList(db.Title, "Title_ID", "Title_Name", user.User_TitleID);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "User_ID,User_Name,User_Password,User_TitleID,User_Address,User_Phone")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_TitleID = new SelectList(db.Title, "Title_ID", "Title_Name", user.User_TitleID);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.User.Find(id);
+            db.User.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Pizza/Validations/ValidationsRegister.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pizza.Validations.Models
+{
+    public partial class VUser
+    {
+        [Display(Name = "รหัส")]
+        [Required(ErrorMessage = "การุณาป้อนข้อมูล")]
+        public int User_ID { get; set; }
+
+
+        [Display(Name = "ชื่อผู้ใช้")]
+        [Required(ErrorMessage = "การุณาป้อนข้อมูล")]
+        public string User_Name { get; set; }
+
+
+        [Display(Name = "รหัสผ่านผู้ใช้")]
+        [Required(ErrorMessage = "การุณาป้อนข้อมูล")]
+        public string User_Password { get; set; }
+
+
+        [Display(Name = "คำนำหน้าชื่อ")]
+        public string User_TitleID { get; set; }
+
+
+        [Display(Name = "ที่อยู่ผู้ใช้")]
+        [Required(ErrorMessage = "การุณาป้อนข้อมูล")]
+        public string User_Address { get; set; }
+
+
+        [Display(Name = "เบอร์มือถือผู้ใช้")]
+        [Required(ErrorMessage = "การุณาป้อนข้อมูล")]
+        public string User_Phone { get; set; }
+    }
+    [MetadataType(typeof(VUser))]
+    public partial class Users { }
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+}
